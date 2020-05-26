988c80afe357c7397ac8bb8fd3c560c25815da56
Daniel Pyon <daniel@quicksat.com>
2020-05-26 14:01:05 -0700

    added SWR

--- pathways/pages/profile.tsx
+++ pathways/pages/profile.tsx
@@ -10,41 +10,15 @@ import UserExperiencesCard from '../components/UserExperiencesCard';
 import UserActivityCard from '../components/UserActivityCard';
 import useSWR from 'swr';
 
-export default class Profile extends React.Component<{}, {bio: string, username: string, email: string, education: string, classOf: number, friendIds: [string], postIds: [string], opportunities: [string], profilePic: string, error: Boolean}> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bio: '',
-            username: '',
-            email: '',
-            education: '',
-            classOf: new Date().getFullYear(),
-            friendIds: [''],
-            postIds: [''],
-            opportunities: [''],
-            profilePic: '',
-            error: false
-        };
+const fetcher = url => fetch(url, {
+    method: 'GET',
+    headers: {
+        'authorization': 'Bearer ' + localStorage.getItem('token')
     }
+}).then(r => r.json());
 
-    /*
-    static async getInitialProps({req}) {
-        if(req) return {};
-        const res = await fetch('http://pathwaysserver.herokuapp.com/getUserInfo', {
-            method: 'GET',
-            headers: {
-                'authorization': 'Bearer ' + localStorage.getItem('token')
-            }
-        });
-        const data = await res.json();
-        
-        console.log(data);
-    
-        return {};
-    }
-    */
-
-    componentDidMount() {
+function Profile() {
+    /*componentDidMount() {
         const headers = new Headers();
         if(localStorage.getItem('token') === null) 
             this.setState({error: true});
@@ -71,59 +45,54 @@ export default class Profile extends React.Component<{}, {bio: string, username:
                     });
                 });
         }
-    }
+    }*/
 
-    render(){
-        if(this.state.error) 
-            return (
-                <div>
-                    <h1 style={{textAlign: 'center'}}>Please log in!</h1>
-                </div>
-            );
+    const { data, error } = useSWR('http://pathwaysserver.herokuapp.com/getUserInfo', fetcher);
+    
+    if(error) return <div>Error loading data.</div>;
+    if(!data) return <div>Loading...</div>;
+    return (
+        <div className="everything">
+            <Head>
+                <title>Profile screen</title>
+            </Head>
+            <Navbar/>
+            <Container fluid className="">
+                <Row>
 
-        return (
-            <div className="everything">
-                <Head>
-                    <title>Profile screen</title>
-                </Head>
-                <Navbar/>
-                <Container fluid className="">
-                    <Row>
+                </Row>
+                <Row>
+                    <Col lg="4">
+                        <UserInfoCard bio={data.bio} education={data.education} classOf={data.classOf} username={data.username} profilePic={data.profilePic} />
+                        <SocialInfoCard/>
+                    </Col>
+                    <Col lg="1">
 
-                    </Row>
-                    <Row>
-                        <Col lg="4">
-                            <UserInfoCard bio={this.state.bio} education={this.state.education} classOf={this.state.classOf} username={this.state.username} profilePic={this.state.profilePic} />
-                            <SocialInfoCard/>
-                        </Col>
-                        <Col lg="1">
-
-                        </Col>
-                        <Col lg="7">
-                            <UserExperiencesCard/>
-                            <UserActivityCard/>
-                        </Col>
-                    </Row>
-                </Container>
-                <style jsx global>{`
-           
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-        .everything{
-            min-height: 100vh;
-            background: linear-gradient(to bottom, rgba(255,140, 0,1) 20%, rgba(252,191,116,1) 100%);
-            background-position: fixed;
-        }
-        .comfortaa{
+                    </Col>
+                    <Col lg="7">
+                        <UserExperiencesCard/>
+                        <UserActivityCard/>
+                    </Col>
+                </Row>
+            </Container>
+            <style jsx global>{`
         
-      `}</style>
-            </div>
-        )
-    }
-
+                @media (max-width: 600px) {
+                .grid {
+                    width: 100%;
+                    flex-direction: column;
+                }
+                }
+                .everything{
+                    min-height: 100vh;
+                    background: linear-gradient(to bottom, rgba(255,140, 0,1) 20%, rgba(252,191,116,1) 100%);
+                    background-position: fixed;
+                }
+                .comfortaa{
+                
+            `}</style>
+        </div>
+    );
 }
 
+export default Profile;
